95f66754a462c926628f09d5dee7129bc24a7cf3
Oleksii Karzov <alex@Alexs-MacBook-Pro.local>
2017-11-27 21:31:50 +0200

    added SwapReorderingCollectionView

---
+++ SwapReorderingCollectionViewDemo/SwapReorderingCollectionView.swift
@@ -0,0 +1,309 @@
+//
+//  SwapReorderingCollectionView.swift
+//  SwapReorderingCollectionViewDemo
+//
+//  Created by Oleksii Karzov on 11/27/17.
+//  Copyright © 2017 olekar. All rights reserved.
+//
+
+import UIKit
+
+
+public typealias SwapReorderingCollectionViewAnimationDataSource = (UICollectionView, IndexPath, UIView, (()->Void)?) -> Void
+
+
+public class SwapReorderingCollectionView: UICollectionView {
+    
+    // MARK: public ivars
+    
+    public var isSwapReordering = true
+    
+    public var maxScrollingSpeed: UInt = 500
+    public var scrollInsets: UIEdgeInsets?
+    
+    public var createViewForInteractiveMovement: (UICollectionView, IndexPath) -> UIView = {
+        return { collectionView, indexPath in
+            let imageView = UIImageView()
+            
+            if let cell = collectionView.cellForItem(at: indexPath) {
+                UIGraphicsBeginImageContextWithOptions(cell.bounds.size, false, 0)
+                cell.layer.render(in: UIGraphicsGetCurrentContext()!)
+                let image = UIGraphicsGetImageFromCurrentImageContext()
+                UIGraphicsEndImageContext()
+                
+                imageView.image = image
+                imageView.frame = cell.frame
+            }
+            
+            return imageView
+        }
+    }()
+    
+    public var beginInteractiveMovementAnimation: SwapReorderingCollectionViewAnimationDataSource = {
+        return { collectionView, indexPath, interactiveView, completion in
+            UIView.animate(withDuration: 0.2, animations: {
+                interactiveView.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+            }, completion: { (finished) in
+                completion?()
+            })
+            } as SwapReorderingCollectionViewAnimationDataSource
+    }()
+    
+    public var finishInteractiveMovementAnimation: SwapReorderingCollectionViewAnimationDataSource = {
+        return { collectionView, indexPath, interactiveView, completion in
+            if let frame = collectionView.collectionViewLayout.layoutAttributesForItem(at: indexPath)?.frame {
+                UIView.animate(withDuration: 0.2, animations: {
+                    interactiveView.transform = CGAffineTransform.identity
+                    interactiveView.center = CGPoint(x: frame.origin.x + frame.size.width / 2,
+                                                     y: frame.origin.y + frame.size.height / 2)
+                }) { (finished) in
+                    completion?()
+                }
+            } else {
+                completion?()
+            }
+            } as SwapReorderingCollectionViewAnimationDataSource
+    }()
+    
+    // MARK: private ivars
+    
+    fileprivate var interactiveCellIndexPath : IndexPath?
+    
+    fileprivate var interactiveView : UIView?
+    
+    fileprivate var scrollingSpeed = CGPoint.zero
+    fileprivate var displayLink: CADisplayLink?
+    
+    // MARK: override
+    
+    override public var contentOffset: CGPoint {
+        didSet {
+            // New position of interactiveView has to be calculated here because contentOffset can be changed from outside
+            // For example, after resetting collectionViewLayout
+            if let currentPosition = interactiveView?.center {
+                let contentOffsetChange = CGPoint(x: contentOffset.x - oldValue.x, y: contentOffset.y - oldValue.y)
+                let newPosition = CGPoint(x: currentPosition.x + contentOffsetChange.x, y: currentPosition.y + contentOffsetChange.y)
+                
+                changeInteractiveViewPosition(to: newPosition)
+            }
+        }
+    }
+    
+    override public func dequeueReusableCell(withReuseIdentifier identifier: String, for indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = super.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        
+        if let interactiveCellIndexPath = interactiveCellIndexPath {
+            cell.isHidden = (indexPath == interactiveCellIndexPath)
+        } else {
+            cell.isHidden = false
+        }
+        
+        return cell
+    }
+    
+    override public func beginInteractiveMovementForItem(at indexPath: IndexPath) -> Bool {
+        guard isSwapReordering else {
+            return super.beginInteractiveMovementForItem(at: indexPath)
+        }
+        
+        if let canMove = dataSource?.collectionView?(self, canMoveItemAt: indexPath), canMove == false {
+            return false
+        }
+        
+        guard interactiveCellIndexPath == nil else {
+            return false
+        }
+        
+        interactiveCellIndexPath = indexPath
+        
+        interactiveView = createViewForInteractiveMovement(self, indexPath)
+        addSubview(interactiveView!)
+        bringSubview(toFront: interactiveView!)
+        
+        beginInteractiveMovementAnimation(self, indexPath, interactiveView!, nil)
+        
+        let interactiveCell = cellForItem(at: indexPath)
+        interactiveCell?.isHidden = true
+        
+        return true
+    }
+    
+    override public func updateInteractiveMovementTargetPosition(_ targetPosition: CGPoint) {
+        guard interactiveCellIndexPath != nil else {
+            return super.updateInteractiveMovementTargetPosition(targetPosition)
+        }
+        
+        changeInteractiveViewPosition(to: targetPosition)
+    }
+    
+    override public func endInteractiveMovement() {
+        guard interactiveCellIndexPath != nil else {
+            return super.endInteractiveMovement()
+        }
+        
+        finishMovement()
+    }
+    
+    override public func cancelInteractiveMovement() {
+        guard interactiveCellIndexPath != nil else {
+            return super.cancelInteractiveMovement()
+        }
+        
+        finishMovement()
+    }
+    
+    // MARK: private
+    
+    fileprivate func changeInteractiveViewPosition(to targetPosition: CGPoint) {
+        guard let interactiveView = interactiveView else {
+            cleanup()
+            return
+        }
+        
+        interactiveView.center = targetPosition
+        
+        reorderIfNeeded()
+        scrollIfNeeded()
+    }
+    
+    fileprivate func finishMovement() {
+        if let interactiveCellIndexPath = interactiveCellIndexPath,
+            let interactiveView = interactiveView {
+            finishInteractiveMovementAnimation(self, interactiveCellIndexPath, interactiveView, { [weak self] in
+                self?.cleanup()
+            })
+        } else {
+            cleanup()
+        }
+    }
+    
+    fileprivate func cleanup() {
+        if let interactiveCellIndexPath = interactiveCellIndexPath,
+            let cell = cellForItem(at: interactiveCellIndexPath) {
+            cell.isHidden = false
+        }
+        
+        interactiveView?.removeFromSuperview()
+        interactiveView = nil
+        
+        interactiveCellIndexPath = nil
+        
+        displayLink?.invalidate()
+        displayLink = nil
+    }
+    
+}
+
+// MARK: - reordering
+
+extension SwapReorderingCollectionView {
+    
+    fileprivate func reorderIfNeeded() {
+        guard let interactiveView = interactiveView,
+            let currentIndexPath = interactiveCellIndexPath,
+            let newIndexPath = indexPathForItem(at: interactiveView.center),
+            currentIndexPath != newIndexPath else {
+                return
+        }
+        
+        notifyDataSourcesTo(swapItemAt: currentIndexPath, withItemAt: newIndexPath)
+        self.interactiveCellIndexPath = newIndexPath
+        
+        performBatchUpdates({
+            self.moveItem(at: currentIndexPath, to: newIndexPath)
+            self.moveItem(at: newIndexPath, to: currentIndexPath)
+        }, completion: {(finished) in
+            
+        })
+    }
+    
+    fileprivate func notifyDataSourcesTo(swapItemAt indexPath1: IndexPath, withItemAt indexPath2: IndexPath) {
+        dataSource?.collectionView?(self, moveItemAt: indexPath1, to: indexPath2)
+        
+        if indexPath1.section != indexPath2.section {
+            let newIndexPath = IndexPath(item: indexPath2.item + 1, section: indexPath2.section)
+            dataSource?.collectionView?(self, moveItemAt: newIndexPath, to: indexPath1)
+        } else {
+            if indexPath1.item < indexPath2.item &&
+                (indexPath1.item != indexPath2.item + 1 && indexPath1.item != indexPath2.item - 1) {
+                let newIndexPath = IndexPath(item: indexPath2.item - 1, section: indexPath2.section)
+                dataSource?.collectionView?(self, moveItemAt: newIndexPath, to: indexPath1)
+            } else if indexPath1.item > indexPath2.item &&
+                (indexPath2.item != indexPath1.item + 1 && indexPath2.item != indexPath1.item - 1) {
+                let newIndexPath = IndexPath(item: indexPath2.item + 1, section: indexPath2.section)
+                dataSource?.collectionView?(self, moveItemAt: newIndexPath, to: indexPath1)
+                
+            }
+        }
+    }
+    
+}
+
+
+// MARK: - scrolling
+
+extension SwapReorderingCollectionView {
+    
+    fileprivate func scrollIfNeeded() {
+        guard let interactiveView = interactiveView else {
+            cleanup()
+            return
+        }
+        
+        let currentPosition = interactiveView.center
+        let interactiveViewBounds = interactiveView.bounds
+        let scrollInset: UIEdgeInsets = scrollInsets ?? UIEdgeInsets(top: interactiveViewBounds.height / 2,
+                                                                     left: interactiveViewBounds.width / 2,
+                                                                     bottom: interactiveViewBounds.height / 2,
+                                                                     right: interactiveViewBounds.width / 2)
+        let maxScrollingSpeed = CGFloat(self.maxScrollingSpeed)
+        var newScrollingSpeed = CGPoint.zero
+        let eps: CGFloat = 0.001
+        
+        if abs(bounds.size.width - contentSize.width) > eps {
+            if currentPosition.x < contentOffset.x + eps + scrollInset.left &&
+                contentOffset.x > 0 + eps {
+                newScrollingSpeed.x = -maxScrollingSpeed
+            } else if currentPosition.x > contentOffset.x + bounds.size.width - eps - scrollInset.right &&
+                contentOffset.x < contentSize.width - bounds.size.width - eps {
+                newScrollingSpeed.x = maxScrollingSpeed
+            }
+        }
+        
+        if abs(bounds.size.height - contentSize.height) > eps {
+            if currentPosition.y < contentOffset.y + eps + scrollInset.top &&
+                contentOffset.y > 0 + eps {
+                newScrollingSpeed.y = -maxScrollingSpeed
+            } else if currentPosition.y > contentOffset.y + bounds.size.height - eps - scrollInset.bottom &&
+                contentOffset.y < contentSize.height - bounds.size.height - eps {
+                newScrollingSpeed.y = maxScrollingSpeed
+            }
+        }
+        
+        scrollingSpeed = newScrollingSpeed
+        
+        if newScrollingSpeed.equalTo(CGPoint.zero) {
+            displayLink?.invalidate()
+            displayLink = nil
+        } else if displayLink == nil {
+            displayLink = CADisplayLink(target: self, selector: #selector(onDisplayLink(_:)))
+            displayLink?.add(to: RunLoop.main, forMode: .commonModes)
+        }
+    }
+    
+    @objc fileprivate func onDisplayLink(_ displayLink: CADisplayLink) {
+        guard interactiveView != nil else {
+            cleanup()
+            return
+        }
+        
+        let translation = CGPoint(x: scrollingSpeed.x * CGFloat(displayLink.duration), y: scrollingSpeed.y * CGFloat(displayLink.duration))
+        
+        var newContentOffset = CGPoint(x: contentOffset.x + translation.x, y: contentOffset.y + translation.y)
+        newContentOffset.x = max(0, min(contentSize.width - bounds.size.width, newContentOffset.x))
+        newContentOffset.y = max(0, min(contentSize.height - bounds.size.height, newContentOffset.y))
+        
+        contentOffset = newContentOffset
+    }
+    
+}
--- SwapReorderingCollectionViewDemo/ViewController.swift
+++ SwapReorderingCollectionViewDemo/ViewController.swift
@@ -9,17 +9,118 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: SwapReorderingCollectionView!
+    
+    var dataArray = (1...50).map{$0}
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print(dataArray)
+        
+//        collectionView.isSwapReordering = false
+        setupCollectionView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+//        setupCollectionView()
     }
-
+    
+    fileprivate func setupCollectionView() {
+        let baseSize = collectionView.bounds
+        let numberOfCellsInRow = (UIDevice.current.userInterfaceIdiom == .pad) ? 6 : 4
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 10
+        let emptySpaceInRow = layout.sectionInset.left + layout.sectionInset.right + CGFloat(numberOfCellsInRow - 1) * layout.minimumInteritemSpacing
+        let cellWidth = (baseSize.width - emptySpaceInRow) / CGFloat(numberOfCellsInRow)
+        layout.itemSize = CGSize(width: cellWidth, height: cellWidth)
+        
+        collectionView.collectionViewLayout = layout
+    }
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        if let label = cell.contentView.subviews.first as? UILabel {
+            let value = dataArray[indexPath.item]
+            
+            label.text = "\(value)"
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let item = dataArray.remove(at: sourceIndexPath.item)
+        dataArray.insert(item, at: destinationIndexPath.item)
+        
+//        print(dataArray)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+}
+
+extension ViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard let longPress = gestureRecognizer as? UILongPressGestureRecognizer else {
+            return true
+        }
+        
+        let location = longPress.location(in: collectionView)
+        
+        if let indexPath = collectionView.indexPathForItem(at: location),
+            let cell = collectionView.cellForItem(at: indexPath) {
+            return cell.contentView.subviews.first is UILabel
+        }
+        
+        return false
+    }
+    
+    @IBAction func onLongPress(_ sender: UILongPressGestureRecognizer) {
+        if #available(iOS 9.0, *) {
+            let location = sender.location(in: collectionView)
+            
+            switch(sender.state) {
+            case .began:
+                guard let indexPath = collectionView.indexPathForItem(at: location) else {
+                    break
+                }
+                
+                let res = collectionView.beginInteractiveMovementForItem(at: indexPath)
+                
+                print("Dragging for item at index \(indexPath) has started - \(res)")
+                
+            case .changed:
+                collectionView.updateInteractiveMovementTargetPosition(location)
+                
+            case .ended:
+                collectionView.updateInteractiveMovementTargetPosition(location)
+                collectionView.endInteractiveMovement()
+                
+                print(dataArray)
+                
+            default:
+                collectionView.cancelInteractiveMovement()
+            }
+        }
+    }
+    
+}
