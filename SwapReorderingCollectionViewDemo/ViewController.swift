fd3f13645e84c8af2da30005fbecf8e3596b6d99
Oleksii Karzov <alex@Alexs-MacBook-Pro.local>
2017-11-27 21:04:01 +0200

    Initial Commit

---
+++ SwapReorderingCollectionViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwapReorderingCollectionViewDemo
+//
+//  Created by Oleksii Karzov on 11/27/17.
+//  Copyright © 2017 olekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
